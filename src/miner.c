95d318749bfe738dd3da08c82cd99b77fef9b3fa
oscariquelme <oscariquelmejato@gmail.com>
2021-04-29 16:54:59 +0200

    Miner can launch workers and work efficiently

--- src/miner.c
+++ src/miner.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <pthread.h>
+
 
 #include "miner.h"
 
@@ -14,6 +16,14 @@
 #define BIG_X 435679812
 #define BIG_Y 100001819
 
+typedef struct _workerInfo {
+    int target; /*Target to be found in the pow*/
+    int numWorkers; /*argv[1]*/
+    int numThread;/*Identifier comprehended betweeen 1 and argv[2]*/
+    int solution;
+} WorkerInfo;
+
+static volatile int solution_found;
 
 long int simple_hash(long int number) {
     long int result = (number * BIG_X + BIG_Y) % PRIME;
@@ -34,7 +44,8 @@ void print_blocks(Block *plast_block, int num_wallets) {
     printf("A total of %d blocks were printed\n", i);
 }
 
-int open_shmemory(){
+int open_netShmemory(){
+    
     int fd;
     if((fd = shm_open(SHM_NAME_NET, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR)) == -1){
         if(errno == EEXIST){
@@ -63,25 +74,79 @@ int open_shmemory(){
     }
 }
 
-int main(int argc, char *argv[]) {
-    long int i, target;
+NetData* map_netShmemory(int fd){
 
+    NetData* data = NULL;
+
+    if ((data = (NetData*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+        perror("mmap");
+        return NULL;
+    }
+    return data;
+}
+
+/*Creo que block debería de ser la memoría compartida de los bloques*/
+void* worker(void* args){
+        
+        int i;
+        int intervalSize;
+        int iniInterval;
+        WorkerInfo *pow = (WorkerInfo*)args;
+
+        /*Gets the interval in which the worker will be working*/
+        intervalSize = PRIME/pow->numWorkers;
+        intervalSize++;
+        iniInterval = intervalSize * pow->numThread;
+
+        for(i = iniInterval; i < iniInterval+intervalSize && solution_found == 0; i++){
+            if(pow->target == simple_hash(i)){
+                solution_found = 1;
+                pow->solution = i;
+                pthread_exit(pow);
+            }
+        }
+        return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    long int i;
+    int solution;
+    pthread_t workers[MAX_WORKERS];
+    short numWorkers;
+    WorkerInfo info[MAX_WORKERS];
+
+    
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <TARGET>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <NUM_WORKERS>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    target = atol(argv[1]);
+    numWorkers = atoi(argv[1]);
+    if(numWorkers > 10 || numWorkers < 1){
+        fprintf(stderr, "Manito ponme al menos 1 trabajador y no mas de 10\n");
+        exit(EXIT_FAILURE);
+    }
 
-    for (i = 0; i < PRIME; i++) {
-        fprintf(stdout, "Searching... %6.2f%%\r", 100.0 * i / PRIME);
-        if (target == simple_hash(i)) {
-            fprintf(stdout, "\nSolution: %ld\n", i);
-            exit(EXIT_SUCCESS);
+    for (i = 0; i < numWorkers; i++){
+        info[i].numWorkers = numWorkers;
+        info[i].target = 1000;
+        info[i].solution = -1;
+        info[i].numThread = i;
+        if(pthread_create(&workers[i], NULL, worker, &info[i]) != 0){
+            fprintf(stderr, "Manito fracase creando hilos...\n");
+            exit(EXIT_FAILURE);
         }
     }
-    fprintf(stderr, "\nSearch failed\n");
-    exit(EXIT_FAILURE);
+
+    for(i = 0; i < numWorkers; i++){
+        pthread_join(workers[i], NULL);
+        if(info[i].solution != -1 ) solution = info[i].solution;
+    }
+
+    fprintf(stdout, "Solucion: %d\n", solution);
+    exit(EXIT_SUCCESS); 
+        
+    
 }
 
 
